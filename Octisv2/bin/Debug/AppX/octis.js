142815d558bd630e2bcac086f644b0f2485073ab
Connor Jarnagin <jarnaginconnor52@gmail.com>
2021-07-18 15:33:05 -0600

    Fixed Rotation/ Tinkered with blocks
    -Fixed up rotation, its way cleaner now, with a nice smooth transition and most importantly, its far less breakable
    -Started tinkering with block generation stuff. Can generate a block in the center with Spacebar

--- Octisv2/bin/Debug/AppX/octis.js
+++ Octisv2/bin/Debug/AppX/octis.js
@@ -1,7 +1,20 @@
 ﻿var camera, camradius;
 var baseWidth = 16;
 var baseHeight = 16;
+var halfHeight;
 var camposition = 0;
+var run = false;
+var xstat;
+var zstat;
+var rotcount = 0;
+var initclock = new Date().getTime();
+var sclock;
+var localx;
+var localz;
+var ignoreInput = false;
+var cyclelength = 20;//miliseconds
+var check = false;
+
 var scenes = [
     new THREE.AmbientLight((intensity = 1)),
     new THREE.AmbientLight((intensity = 0.7)),
@@ -16,6 +29,9 @@ document.addEventListener("keydown", handleKeyDown);
 //////////////////////////////////////////////////////////////////////
 function init()
 {
+    /************VARIABLE-INITS****************/
+    halfHeight = baseHeight / 2;
+
     /*****CAMERA VARIABLE INITIILIZATIONS******/
     sceneMain = new THREE.Scene();
     camera = new THREE.PerspectiveCamera
@@ -26,7 +42,7 @@ function init()
         1000
     );
     camera.position.set(50, 40, 0);
-    camera.lookAt(new THREE.Vector3(0, baseHeight / 2, 0));
+    camera.lookAt(new THREE.Vector3(0, halfHeight, 0));
     camradius = Math.sqrt
     (
         Math.pow(camera.position.x, 2) +
@@ -39,31 +55,6 @@ function init()
 
     /****************CREATING THE GRID****************/
     var lineCol = new THREE.LineBasicMaterial({ color: 0xffffff });//initilizing the color of the lines
-    var halfHeight = baseHeight / 2;
-    for (x = -(baseWidth / 2); x < (baseWidth / 2); x++)//each unit is == to 16/16 or basewidth/basewidth this starts from half increments...
-    // so the minimum would be -8? so thats -(basewidth/2)
-    {
-        // Create vertical lines
-        for (i = -halfHeight; i <= halfHeight; i++)
-        {
-            var geometry = new THREE.Geometry();
-            geometry.vertices.push(new THREE.Vector3(x, baseHeight, i));
-            geometry.vertices.push(new THREE.Vector3(x, 0, i));
-            var line = new THREE.Line(geometry, lineCol);
-            sceneMain.add(line);
-        }
-        // Create horizonal lines
-        /*
-        for (i = 0; i <= halfHeight * 1.5; i++)
-        {
-            var geometry = new THREE.Geometry();
-            geometry.vertices.push(new THREE.Vector3(x, i, -14.5));
-            geometry.vertices.push(new THREE.Vector3(x, i, 14.5));
-            var line = new THREE.Line(geometry, lineCol);
-            sceneMain.add(line);
-        }
-        */
-    }
     /*************************************************/
 
 
@@ -163,6 +154,8 @@ function animate()
 /////////////////////////////////////////////////////////
 function handleKeyDown(event)
 {
+    if (!ignoreInput)
+    {
         switch (event.keyCode)
         {
             case 81: // Q key
@@ -171,14 +164,15 @@ function handleKeyDown(event)
             case 69: // E key
                 rotateGrid(false);
                 break;
+            case 32: //SPACE
+                createCube();
         }
+    }
 }
 /////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////
 function rotateGrid(leftorright)//true or false
 {
-    var xstat;
-    var zstat;
     switch (camposition)
     {
         case 0:
@@ -198,32 +192,66 @@ function rotateGrid(leftorright)//true or false
             zstat = true;
             break;
     }
-
     leftorright ? camposition-- : camposition++;
     camposition < 0 ? camposition = 3 : null;
     camposition > 3 ? camposition = 0 : null;
-
-
-    var localx = camera.position.x;
-    var localz = camera.position.z;
-    for (x = 0; x < 50; x++)//rotation loop for smooth transitions
-    {
-        setTimeout(
-            function exec()
-            {
-                xstat ? localx++ : localx--;
-                zstat ? localz++ : localz--;
-                camera.position.set(localx, 40, localz);
-                camera.lookAt(new THREE.Vector3(0, baseHeight / 2, 0));
-                renderer.clear();
-                for (j = 0; j < scenes.length; j++)
-                {
-                    renderer.render(scenes[j], camera);
-                }
-            }
-            ,0);
-    }
+    localx = camera.position.x;
+    localz = camera.position.z;
+    rotcount = 0;
+    run = true;
 }
+/////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
+setInterval(function isRotating()
+{
+    if (run)//time based rotation loop for smooth transitions
+    {
+        ignoreInput = true;
+        sclock = new Date().getTime();
+        if (sclock - initclock > cyclelength)
+        {
+            check = true;
+        }
+        if (check)
+        {
+            xstat ? localx++ : localx--;
+            zstat ? localz++ : localz--;
+            camera.position.set(localx, 40, localz);
+            camera.lookAt(new THREE.Vector3(0, halfHeight, 0));
+            renderer.clear();
+            for (j = 0; j < scenes.length; j++)
+            {
+                renderer.render(scenes[j], camera);
+            }
+            rotcount++;
+            check = false;
+            if (rotcount > 49) { run = false; ignoreInput = false; }
+        }
+    }
+}, 5);
+/////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
+function createCube()
+{
+    var geometry = new THREE.BoxGeometry(1, 1, 1);
+    var col = getRandomColor();
+    var material = new THREE.MeshPhongMaterial({ color: col });
+    var cube = new THREE.Mesh(geometry, material);
+    sceneMain.add(cube);
+    cube.position.set(0, 0, 0);
 
+}
+/////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
+function getRandomColor()
+{
+    var letters = "0123456789ABCDEF";
+    var col = "#";
+    for (var i = 0; i < 6; i++)
+    {
+        col += letters[Math.floor(Math.random() * 16)];
+    }
+    return col;
+}
 /////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////
